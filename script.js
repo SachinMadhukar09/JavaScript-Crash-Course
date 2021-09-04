ea07ee938c829db9b086a0e58d3296e8e21d05eb
Sachin Madhukar <sachinkumarmadhukar@gmail.com>
2021-09-04 23:41:03 +0530

    Working on Blackjack Challange Front End Done

--- script.js
+++ script.js
@@ -196,3 +196,42 @@ function buttonRandom() {
     all_buttons[i].classList.add(choice[randomNumber]);
   }
 }
+
+// Challenge 6 : Blackjack
+let blackjackGame = {
+  you: { scoreSpan: "#your-blackjack-result", div: "#your-box", score: 0 },
+  dealer: {
+    scoreSpan: "#dealer-blackjack-result",
+    div: "#dealer-box",
+    score: 0,
+  },
+};
+
+const YOU = blackjackGame["you"];
+const DEALER = blackjackGame["dealer"];
+
+const hitSound = new Audio("../sounds/swish.m4a");
+
+document
+  .querySelector("#blackjack-hit-button")
+  .addEventListener("click", blackjackHit);
+
+document
+  .querySelector("#blackjack-deal-button")
+  .addEventListener("click", blackjackDeal);
+
+function blackjackHit() {
+  showCard(YOU);
+}
+
+function showCard(activePlayer) {
+  let cardImage = document.createElement("img");
+  cardImage.src = "../images/Q.png";
+  document.querySelector(activePlayer["div"]).appendChild(cardImage);
+  hitSound.play();
+}
+
+function blackjackDeal() {
+  let yourImage = document.querySelector("#your-box").querySelectorAll("img");
+  yourImage[0].remove();
+}
